4f5393498bc2b224fffc541dfc2bd49e7f14221b
isan-san <isan.9.9f@gmail.com>
2023-03-21 20:27:30 -0500

    Create Queue event

---
+++ src/main/java/ec/com/portfolio/sofkaU/api/publisher/PortfolioEvent.java
@@ -0,0 +1,12 @@
+package ec.com.portfolio.sofkaU.api.publisher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class PortfolioEvent {
+    private String projectID;
+    private String eventType;
+}
