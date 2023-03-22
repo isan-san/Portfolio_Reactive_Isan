96d9122524dfab2ca19d86c25fd8ea87967a83d7
isan-san <isan.9.9f@gmail.com>
2023-03-21 20:28:03 -0500

    Create protfolio publisher

---
+++ src/main/java/ec/com/portfolio/sofkaU/api/publisher/PortfolioPublisher.java
@@ -0,0 +1,32 @@
+package ec.com.portfolio.sofkaU.api.publisher;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ec.com.portfolio.sofkaU.api.domain.dto.PortfolioDTO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortfolioPublisher {
+    private final RabbitTemplate rabbitTemplate;
+    //private final DirectExchange exchange;
+    private final ObjectMapper objectMapper;
+
+
+    public PortfolioPublisher(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.rabbitTemplate = rabbitTemplate;
+        //this.exchange = exchange;
+    }
+
+    public void publish(PortfolioDTO bookDTO, String id) throws JsonProcessingException {
+        //String message = objectMapper.writeValueAsString("Book with id "+bookDTO.getId()+" has been lended to student with id "+id);
+        String message = objectMapper.writeValueAsString(new PortfolioEvent(id,"Project.Append"));
+        rabbitTemplate.convertAndSend("books-exchange-events", "events.book.routing.key", message);
+
+    }
+
+
+    public void publishError(Throwable errorEvent){
+    }
+}
